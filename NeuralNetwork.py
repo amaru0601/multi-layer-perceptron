b936907f78670b1589f346756a0e6997563cbb75
jaoks <jaoks@pop-os.localdomain>
2022-01-10 19:22:03 -0500

    fix hot encoding

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -11,11 +11,14 @@ df = pd.read_csv('data/sign_mnist_train.csv')
 imgs =df.iloc[:,1:] #separamos las imagenes de los labels
 y = df.iloc[:,0:1] #capturamos los labels
 y = y.to_numpy()
+unique, counts = np.unique(y, return_counts=True)
+#print(len(unique))
+#print(np.asarray((unique, counts)).T)
 
-#one hot encoding a los labels usamos 25 porque en el dataset no esta presenta la letra Z
-y_train = np.zeros((27455, 25))
+#no cases for 9=J or 25=Z because of gesture motions
+y_train = np.zeros((len(y), 26))
 for i, y in enumerate(y):
-    y_train[i][y-1] = 1
+    y_train[i][y] = 1
 #print(y.shape)
 #print(y_train)
 #regularizamos los valores del train set
@@ -23,14 +26,6 @@ mean = np.mean(imgs)
 stdev = np.std(imgs)
 x_train = (imgs - mean) / stdev
 x_train = x_train.to_numpy()
-
-
-
-
-#agregamos una columna con valor 1 al train set para el baias
-#x_train = np.insert( x_train, 0, 1, axis = 1)
-#print(x_train.shape)
-#print(x_train)
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
